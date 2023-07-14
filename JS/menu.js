59522a8f02600fc30618e462083f7f4cce545386
Vitaly <answer-0885@mail.ru>
2023-07-14 23:29:33 +0700

    Бургер завершён

--- JS/menu.js
+++ JS/menu.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.header-body__burger-menu').click(function () {
         $('.header-body__burger-menu').toggleClass('open-menu');
         $('.header-menu').toggleClass('open-menu');
-        $('body').toggleClass('fixed-page');
+
     });
 });
 // Блокировка тела при открытом меню
--- JS/smoothMove.js
+++ JS/smoothMove.js
@@ -1,11 +1,19 @@
 const menuHeader = document.querySelector('.header-menu-list');
 
+
+
 const menu = () => {
+   const body = document.querySelector('body');
+   const burgerMenu = document.querySelector('.header-body__burger-menu');
+   const menu = document.querySelector('.header-menu');
     menuHeader.addEventListener('click', (e) => {
           // Плавно перемещаемся к выбранному блоку из списка меню
         if (menuHeader.contains(e.target)) {
           e.preventDefault();
           const block = e.target.getAttribute('href');
+          body.classList.remove('fixed-page');
+          burgerMenu.classList.remove('open-menu');
+          menu.classList.remove('open-menu');
           document.querySelector(block).scrollIntoView({
              behavior: 'smooth',
              block: 'start'
