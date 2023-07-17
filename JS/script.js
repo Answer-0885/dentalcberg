1770c9c408f2ca027092f191737b5d4a8e6c6d1f
Vitaly <answer-0885@mail.ru>
2023-07-18 01:50:55 +0700

    Работа над 1400 закончена

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ const form2 = document.querySelector('.form-2');
 const buttonVideo = document.querySelector('.main__content_icons-video');
 const buttonVideo2 = document.querySelector('.main__content-4_header_right-video-icons');
 const video = document.querySelector('.main__content_video-video');
+const video2 = document.querySelector('.main__content_video-video-2');
 const overlay = document.querySelector('.overlay');
 const body = document.querySelector('body');
 const buttonQuestions = document.querySelector('.header-menu__button');
@@ -24,21 +25,15 @@ const videoHeader = () => {
         video.play();
 
     });
-    buttonVideo2.addEventListener('click',() => {
-        video.style.display = 'block';
-        overlay.style.display = 'block';
-        // body.style.overflow = 'hidden';
-        video.play();
 
-    });
 
-    // Закрываем и формувидео при нажатии на крестик или оверлай 
+    // Закрываем и форму и видео при нажатии на крестик или оверлай 
     window.addEventListener('click', (e) => {
         if (e.target.closest('.overlay') || e.target.classList.contains('close__video')) {
             video.style.display = 'none';
             overlay.style.display = 'none';
             form2.style.display = 'none';
-            // body.style.overflow = 'auto';
+             // body.style.overflow = 'auto';
             video.pause();
         };
     });
