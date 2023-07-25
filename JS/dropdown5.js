c81de2e1e3ec661ca0d209dd1ad8bb1af2a089e1
Vitaly <answer-0885@mail.ru>
2023-07-26 01:01:31 +0700

    Основные замечания исправил на десктопе

---
+++ JS/dropdown5.js
@@ -0,0 +1,18 @@
+const dropDown5 = () => {
+    // const dropDownList = document.querySelector('.dropdown-5-list');
+    const whatsapp = document.querySelector('.dropdown-5-item-1');
+    const telegram = document.querySelector('.dropdown-5-item-2');
+
+
+    whatsapp.addEventListener('click', () =>{
+        telegram.style.display = 'block'
+        whatsapp.style.display = 'none'
+    })
+
+    telegram.addEventListener('click', () =>{
+        whatsapp.style.display = 'block'
+        telegram.style.display = 'none'
+    })
+
+}
+dropDown5()
