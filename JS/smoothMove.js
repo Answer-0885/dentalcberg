2ca3ec6d5cc7e4e32360ebe1bf1d7d1d58d3cbb1
Vitaly <answer-0885@mail.ru>
2023-07-26 16:26:45 +0700

    Убрал все ошибки из консоли

--- JS/smoothMove.js
+++ JS/smoothMove.js
@@ -3,21 +3,27 @@ const menuHeader = document.querySelector('.header-menu-list');
 
 
 const menu = () => {
-   const body = document.querySelector('body');
-   const burgerMenu = document.querySelector('.header-body__burger-menu');
-   const menu = document.querySelector('.header-menu');
+   const body = document.querySelector('body')
+   const burgerMenu = document.querySelector('.header-body__burger-menu')
+   const menu = document.querySelector('.header-menu')
+
     menuHeader.addEventListener('click', (e) => {
           // Плавно перемещаемся к выбранному блоку из списка меню
         if (menuHeader.contains(e.target)) {
           e.preventDefault();
-          const block = e.target.getAttribute('href');
+          const block = e.target.getAttribute('href') || !e.target.getAttribute('href');
           body.classList.remove('fixed-page');
           burgerMenu.classList.remove('open-menu');
           menu.classList.remove('open-menu');
-          document.querySelector(block).scrollIntoView({
-             behavior: 'smooth',
-             block: 'start'
-          });
+          if(block == e.target.getAttribute('href')) {
+            document.querySelector(block).scrollIntoView({
+               behavior: 'smooth',
+               block: 'start'
+            });
+          } else {
+            return
+          }
+
        } 
     });
  };
