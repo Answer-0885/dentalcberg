5dd8738cc6fdfdc3ab7a34ba1a3f3dae67fc9034
Vitaly <answer-0885@mail.ru>
2023-08-01 18:04:09 +0700

    390 готово

--- JS/swiper11.js
+++ JS/swiper11.js
@@ -1,9 +1,72 @@
 'use strict'
 const swiper11 = () => {
+
     const swiper = new Swiper('.swiper2', {
+      on: {
+         slideChange:  () => {
+            const slides11 = document.querySelector('.slides-11');
+            const slides = document.querySelector('.slide_number .span1');
+            const slidesSpan = document.querySelector('.slide_number .span2');
+            const slideLength = slides11.querySelectorAll('div');
+            const doctors1 = document.querySelector('.doctors.d-1');
+            const doctors2 = document.querySelector('.doctors.d-2');
+            const doctors3 = document.querySelector('.doctors.d-3');
+            const doctors4 = document.querySelector('.doctors.d-4');
+            const doctors5 = document.querySelector('.doctors.d-5');
+
+            let quanitySlide = slideLength.length; 
+            let activeIndex = swiper.activeIndex;
+            let realIndex = swiper.realIndex + 1;
+           
+            slidesSpan.innerHTML = '/ ' + quanitySlide;
+            slides.innerHTML = realIndex;
+
+            if(activeIndex == 0) {
+               console.log(activeIndex);
+               doctors1.style.display = 'flex'
+               doctors2.style.display = 'none'
+               doctors3.style.display = 'none'
+               doctors4.style.display = 'none'
+               doctors5.style.display = 'none'
+            } 
+            else if (activeIndex == 1) {
+               console.log(activeIndex);
+               doctors1.style.display = 'none'
+               doctors2.style.display = 'flex'
+               doctors3.style.display = 'none'
+               doctors4.style.display = 'none'
+               doctors5.style.display = 'none'
+            }
+            else if (activeIndex == 2) {
+               console.log(activeIndex);
+               doctors1.style.display = 'none'
+               doctors2.style.display = 'none'
+               doctors3.style.display = 'flex'
+               doctors4.style.display = 'none'
+               doctors5.style.display = 'none'
+            }
+            else if (activeIndex == 3) {
+               console.log(activeIndex);
+               doctors1.style.display = 'none'
+               doctors2.style.display = 'none'
+               doctors3.style.display = 'none'
+               doctors4.style.display = 'flex'
+               doctors5.style.display = 'none'
+            }
+            else if (activeIndex == 4) {
+               console.log(activeIndex);
+               doctors1.style.display = 'none'
+               doctors2.style.display = 'none'
+               doctors3.style.display = 'none'
+               doctors4.style.display = 'none'
+               doctors5.style.display = 'flex'
+            } else {
+               return
+            }
+         }},
        navigation: {
           nextEl: '.benefits__arrow--right',
-          prevEl: '.benefits__arrow--left',
+         //  prevEl: '.benefits__arrow--left',
        },
        loop: true,
        autoplay: {
